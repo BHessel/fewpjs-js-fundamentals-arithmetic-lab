eee3143d14ef0adf797ce16aae4716c1181418bb
BHessel <hesselfitness@gmail.com>
2020-11-16 21:02:38 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+//1
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+//2
+let random = Math.random();
+//3
+let mod = 16 % 6;
+//4
+let max = Math.max(1, 6, 18, 20);
